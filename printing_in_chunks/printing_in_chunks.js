04dc90c3c675f06f83ba9e97d6c09a3b80f020a7
Madeline Kang <maddyjkang@gmail.com>
2018-08-06 18:37:14 +0000

    printing chunks with line breaks in between chunks

---
+++ printing_in_chunks/printing_in_chunks.js
@@ -0,0 +1,21 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8'); {
+  }
+
+  response.on('data', function (data) {
+    console.log('\n', data);
+  });
+
+response.on('end', function() {
+  console.log('Response stream complete.')
+});
+
+});
